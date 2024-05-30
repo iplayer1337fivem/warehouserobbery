f63a69e97f56aeb00d63a2ab82b629196c6eb0d1
KostaZx <76644544+KostaZx@users.noreply.github.com>
2024-05-30 16:38:40 +0300

    cop check on esx fix

--- bridge/server.lua
+++ bridge/server.lua
@@ -31,20 +31,20 @@ function PlayersWithJob(jobName)
     local jobCount = 0
     
     if framework == 'esx' then
-        for _, player in pairs(ESX.GetExtendedPlayers()) do
-            local job = player.getJob()
-            for _, jobNames in pairs(jobName) do
-                if job.name == jobNames then
-                    jobCount = jobCount + 1
-                end
-            end
-        end
+        jobCount = ESX.GetExtendedPlayers('job', jobName)
     elseif framework == 'qb' then
         for _, playerId in pairs(QBCore.Functions.GetPlayers()) do
             local player = QBCore.Functions.GetPlayer(playerId)
             local job = player.PlayerData.job
-            for _, jobNames in pairs(jobName) do
-                if job.name == jobNames then
+            if type(jobName) == 'table' then
+                for _, jobNames in pairs(jobName) do
+                    if job.name == jobNames then
+                        jobCount = jobCount + 1
+                        break
+                    end
+                end
+            else
+                if job.name == jobName then
                     jobCount = jobCount + 1
                 end
             end
@@ -57,3 +57,4 @@ function PlayersWithJob(jobName)
 
     return jobCount
 end
+
